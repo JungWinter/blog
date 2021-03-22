d462627ecacf0455c6395c5f588fd9d88378d050
JungWinter <JungWinter@users.noreply.github.com>
2021-03-22 09:44:40 +0000

    Deploy blog for c0e966d7d611a43db3b91dfb7734214a799f5930

---
+++ static/e1a6472f7e4b3e457dcde21686ab5db0/index.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default ({ data }) => (
+    <Layout>
+        <SEO />
+        <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+                <div key={node.id}>
+                    <Link
+                        to={node.fields.slug}
+                        css={css`
+                            text-decoration: none;
+                        `}>
+                        <h3
+                            css={css`
+                                margin-bottom: ${rhythm(0.5)};
+                            `}>
+                            {node.frontmatter.title}
+                        </h3>
+                    </Link>
+                    <p>
+                        {node.fields.date} - {node.excerpt}
+                    </p>
+                </div>
+            ))}
+        </div>
+    </Layout>
+)
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(sort: { fields: [fields___date], order: DESC }) {
+            totalCount
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                    }
+                    fields {
+                        slug
+                        date
+                    }
+                    excerpt(truncate: true)
+                }
+            }
+        }
+    }
+`
